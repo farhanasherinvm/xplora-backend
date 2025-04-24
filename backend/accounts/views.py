1b60040bf1eeb072a60107ee60298210946b8d81
farhansherinvm <farhanasherinvm@gmail.com>
2025-04-24 13:15:05 +0530

    JWT auth

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.response import Response
+from rest_framework.views import APIView
 # Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset=User.objects.all()
+    serializer_class=RegisterSerializer
+    permission_classes=(AllowAny,)
+class LogoutView(APIView):
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response({"message": "Logged out successfully."})
+        except Exception as e:
+            return Response({"error": str(e)}, status=400)
